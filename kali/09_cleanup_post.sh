9a3f0c3a6264ae1d583899b4bf21e04ff552a04f
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2019-09-03 17:04:48 +0200

    Initial commit

---
+++ kali/09_cleanup_post.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+# vim: set ts=4 sw=4 sts=4 et :
+
+if [ "$VERBOSE" -ge 2 -o "$DEBUG" == "1" ]; then
+    set -x
+fi
+
+source "${SCRIPTSDIR}/vars.sh"
+source "${SCRIPTSDIR}/distribution.sh"
+
+##### '-------------------------------------------------------------------------
+debug ' Kali post installation cleanup'
+##### '-------------------------------------------------------------------------
